96dec9872bec437f9fa38db9666109e7aea82813
wanmengjie <74043721+wanmengjie@users.noreply.github.com>
2024-10-05 16:18:42 +0800

    Update depression.py

--- depression.py
+++ depression.py
@@ -18,30 +18,30 @@ def make_prediction(model, input_features):
     return prediction[0]
 
 # Streamlit 应用界面
-st.title('抑郁症预测模型')
+st.title('Predicting Depression in Adolescent Girls with Non-Suicidal Self-Injury')
+
 
 # 创建输入控件
-age = st.number_input('Age')
-major_life_events = st.number_input('Major life events')
-negative_thoughts_behaviors = st.number_input('Negative thoughts/behaviors')
-number_suicide_attempts = st.number_input('Number of suicide attempts')
-ipaq_level = st.number_input('IPAQ level')
-bmi = st.number_input('BMI')
-sleep_quality = st.number_input('Sleep quality')
-perceived_stress = st.number_input('Perceived stress')
-hopelessness = st.number_input('Hopelessness')
-loneliness = st.number_input('Loneliness')
-resilience = st.number_input('Resilience')
-alexithymia = st.number_input('Alexithymia')
-problem_focused_coping = st.number_input('Problem-focused coping')
-emotion_focused_coping = st.number_input('Emotion-focused coping')
-self_esteem = st.number_input('Self-esteem')
-rumination = st.number_input('Rumination')
-emotion_regulation = st.number_input('Emotion regulation')
-borderline_personality = st.number_input('Borderline personality')
-care = st.number_input('Care')
-overprotection = st.number_input('Overprotection')
-
+age = st.number_input('Age', min_value=10, max_value=18, step=1)
+major_life_events = st.number_input('Major life events', min_value=0, max_value=1, step=1)
+negative_thoughts_behaviors = st.number_input('Negative thoughts/behaviors', min_value=0, max_value=100, step=1)
+number_suicide_attempts = st.number_input('Number of suicide attempts', min_value=0, max_value=2, step=1)
+ipaq_level = st.number_input('IPAQ level', min_value=1, max_value=3, step=1)
+bmi = st.number_input('BMI', min_value=13, max_value=50, step=1)
+sleep_quality = st.number_input('Sleep quality', min_value=0, max_value=21, step=1)
+perceived_stress = st.number_input('Perceived stress', min_value=4, max_value=20, step=1)
+hopelessness = st.number_input('Hopelessness', min_value=20, max_value=100, step=1)
+loneliness = st.number_input('Loneliness', min_value=23, max_value=80, step=1)
+resilience = st.number_input('Resilience', min_value=0, max_value=40, step=1)
+alexithymia = st.number_input('Alexithymia', min_value=26, max_value=97, step=1)
+problem_focused_coping = st.number_input('Problem-focused coping', min_value=20, max_value=80, step=1)
+emotion_focused_coping = st.number_input('Emotion-focused coping', min_value=17, max_value=66, step=1)
+self_esteem = st.number_input('Self-esteem', min_value=10, max_value=40, step=1)
+rumination = st.number_input('Rumination', min_value=22, max_value=88, step=1)
+emotion_regulation = st.number_input('Emotion regulation', min_value=10, max_value=50, step=1)
+borderline_personality = st.number_input('Borderline personality', min_value=25, max_value=120, step=1)
+care = st.number_input('Care', min_value=0, max_value=36, step=1)
+overprotection = st.number_input('Overprotection', min_value=0, max_value=36, step=1)
 # 当用户点击预测按钮时，显示预测结果
 if st.button('预测'):
     # 创建输入特征列表
